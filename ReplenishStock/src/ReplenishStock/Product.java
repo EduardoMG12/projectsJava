db6170cec13ba72c99c0dba4519649aafe709469
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2022-12-03 11:31:39 -0300

    new exercise

--- ReplenishStock/src/ReplenishStock/Product.java
+++ ReplenishStock/src/ReplenishStock/Product.java
@@ -7,4 +7,28 @@ public class Product {
     String description;
     Integer quantityInStock;
 
+
+
+    Boolean reloadStock() {
+        Boolean outOfStock = true;
+        if (quantityInStock < 10) {
+
+            outOfStock = true;
+
+        }
+        if (quantityInStock > 10) {
+
+            outOfStock = false;
+        }
+        ;
+        if (outOfStock) {
+            System.out.println("Tera que repor o estoque de " + name + " pois apenas existem " + quantityInStock + " em estoque ");
+        }
+        if (!outOfStock) {
+
+            System.out.println("Voce tem " + quantityInStock + " " + name + " em estoque");
+        }
+        return outOfStock;
+
+    }
 }
--- ReplenishStock/src/ReplenishStock/main.java
+++ ReplenishStock/src/ReplenishStock/main.java
@@ -5,29 +5,8 @@ public class main {
         Product product = new Product();
 
         product.name="Cheetos";
-        product.quantityInStock= 11;
-
-        if(reloadStock(product)){
-            System.out.print("Tera que repor o estoque de " + product.name + " pois apenas existem " + product.quantityInStock+ " em estoque ");
-        }
-        if(!reloadStock(product)){
-            System.out.print("Voce tem " + product.quantityInStock + " "+ product.name + " em estoque");
-        }
-
-    }
-    static Boolean reloadStock(Product product){
-        Boolean outOfStock = true;
-        if (product.quantityInStock < 10){
-
-            outOfStock = true;
-
-        }
-        if(product.quantityInStock > 10){
-
-            outOfStock = false;
-        };
-        return outOfStock;
-
+        product.quantityInStock= 9;
+        product.reloadStock();
 
     };
 }
