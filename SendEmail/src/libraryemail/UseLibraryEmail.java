588ce50465609ec97eb2719704eed353537df158
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2022-12-07 16:47:49 -0300

    refactoring exercise

--- SendEmail/src/libraryemail/UseLibraryEmail.java
+++ SendEmail/src/libraryemail/UseLibraryEmail.java
@@ -29,17 +29,22 @@ public static void main(String[] args) throws EmailException, EmailException {
     for(int i = 0; i < Bot.bots.size(); i++) {
         String username = Bot.bots.get(i).email;
         String password = Bot.bots.get(i).password;
+        try {
+            Email email = new SimpleEmail();
+            email.setHostName("smtp.office365.com");
+            email.setSmtpPort(587);
+            email.setAuthenticator(new DefaultAuthenticator(username, password));
+            email.setFrom(username);
+            email.setSubject("Um email teste para fins educativos");
+            email.setMsg(MessageSend.messageSend[i]);
+            email.addTo("");
+            email.setStartTLSEnabled(true);
+            email.send();
+            System.out.println("Email enviado com sucesso!!");
 
-        Email email = new SimpleEmail();
-        email.setHostName("smtp.office365.com");
-        email.setSmtpPort(587);
-        email.setAuthenticator(new DefaultAuthenticator(username, password));
-        email.setFrom(username);
-        email.setSubject("Um email teste para fins educativos");
-        email.setMsg(MessageSend.messageSend[i]);
-        email.addTo("");
-        email.setStartTLSEnabled(true);
-        email.send();
+        }catch (Exception e){
+            System.out.println("Erro o email não foi enviado " + e);
+        }
     }
  }
 }
