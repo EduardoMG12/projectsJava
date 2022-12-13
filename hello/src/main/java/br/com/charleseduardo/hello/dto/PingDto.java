6af6da489ffa5a72ad3f26b6218eaedb48ff8990
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2022-12-13 17:10:53 -0300

    add: Spring boot html

--- hello/src/main/java/br/com/charleseduardo/hello/dto/PingDto.java
+++
@@ -1,6 +0,0 @@
-package br.com.charleseduardo.hello.dto;
-
-public class PingDto {
-    public String algo;
-
-}
--- landing-page-eduardo-m/src/main/java/com/landingPageEduardoM/LandingPageEduardoMApplication.java
+++ landing-page-eduardo-m/src/main/java/com/landingPageEduardoM/LandingPageEduardoMApplication.java
@@ -1,13 +1,13 @@
-package br.com.charleseduardo.hello;
+package com.landingPageEduardoM;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloApplication {
+public class LandingPageEduardoMApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloApplication.class, args);
+		SpringApplication.run(LandingPageEduardoMApplication.class, args);
 	}
 
 }
